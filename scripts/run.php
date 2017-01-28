860046428aa8810d8059296df288c00afcfca6ba
Jeff Puckett <jeff@jeffpuckett.com>
2017-01-28 23:33:18 +0000

    email distributions

--- scripts/run.php
+++ scripts/run.php
@@ -1,7 +1,5 @@
 <?php require_once __DIR__.'/../vendor/autoload.php';
 
-use jpuck\PeerValueDistributer;
-
 $endpoint = 'https://blog.isys4283.walton.uark.edu/wp-json/semantic-scale/v1/leaderboard';
 
 $client = new GuzzleHttp\Client();
@@ -15,5 +13,31 @@ if ( $response->getStatusCode() === 200 ) {
         $blogs[$blog['name']] = $blog['guid'];
     }
 
-    print_r(PeerValueDistributer::distribute($blogs, $count = 3));
+    $distribution = jpuck\PeerValueDistributer::distribute($blogs, $count = 3);
+
+    $dotenv = new Dotenv\Dotenv(dirname(__DIR__));
+    $dotenv->load();
+
+    $mailer = Swift_Mailer::newInstance(
+        Swift_SmtpTransport::newInstance(
+            getenv('MAIL_HOST'),
+            getenv('MAIL_PORT'),
+            getenv('MAIL_ENCRYPTION')
+        )
+        ->setUsername(getenv('MAIL_USERNAME'))
+        ->setPassword(getenv('MAIL_PASSWORD'))
+    );
+
+    $message = "Hello, please review the following blogs:\n\n";
+
+    $message .= "Refer back to this link for directions: https://github.com/ISYS4283/peer-reviews\n\n";
+
+    foreach ( $distribution as $username => $reviews ) {
+        $mailer->send(Swift_Message::newInstance('ISYS 4283 Blog Review')
+            ->setFrom("isys4283@uark.edu")
+            ->setTo("$username@uark.edu")
+            ->setBody($message . print_r($reviews, true))
+        );
+        sleep(1);
+    }
 }
